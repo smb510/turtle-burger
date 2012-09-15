7239a3d04c850ef9faf9be8df0a9c8d6961012cb
Scottie Biddle <smb510@gmail.com>
2012-09-15 15:17:37 -0400

    removed webapp

--- web_server/WorkoutMaker/test/functional/workout_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WorkoutControllerTest < ActionController::TestCase
-  test "should get assemble" do
-    get :assemble
-    assert_response :success
-  end
-
-end
--- web_server/WorkoutMaker/test/unit/helpers/workout_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class WorkoutHelperTest < ActionView::TestCase
-end
