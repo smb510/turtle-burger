17976f77c3b07581eeacf0df2353c8c5c7a4a020
Scottie Biddle <smb510@gmail.com>
2012-09-15 11:42:45 -0400

    added exercise.js

---
+++ exercise.js
@@ -0,0 +1,51 @@
+function WRWorkout(obj)
+{
+	this.duration = obj.duration;
+	this.exercises = obj.exercises;
+}
+
+
+WRWorkout.prototype = {
+	__class__: 'WRWorkout',
+	
+	setDuration : function(duration)
+	{
+		this.duration = duration;
+	},
+	
+	setExercises : function(exercises)
+	{
+		this.exercises = exercises;
+	}
+}
+
+
+
+function WRExercise(obj)
+{
+	this.type = obj.type;
+	this.duration = obj.duration;
+	this.description = obj.description;
+}
+
+WRExercise.prototype = {
+	
+	__class__: 'WRExercise',
+	
+	setDuration : function(duration)
+	{
+		this.duration = duration;
+	},
+	
+	setType : function(type)
+	{
+		this.type = type;
+	},
+	setDescription : function(description)
+	{
+		this.description = description;
+	}
+	
+	
+	
+}
